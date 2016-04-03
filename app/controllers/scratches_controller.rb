33bd94677394ad27f246895a2baa59faf74cb19b
voidzero <kartikjagdale11@gmail.com>
2016-04-03 11:42:52 +0530

    Bootsarp and some usefull gems integration

--- app/controllers/scratches_controller.rb
+++ app/controllers/scratches_controller.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: scratches
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class ScratchesController < ApplicationController
   before_action :find_scratch, only: [:show, :edit, :update, :destroy]
   
